9cc5ac3e265f9fa50a8a04ba1dca69097572bd15
tyler <scottt2@uw.edu>
2019-06-12 07:58:55 -0600

    Add State

---
+++ state/README.md
@@ -0,0 +1,4 @@
+# State Pattern
+The state pattern is used in computer programming to encapsulate varying behavior for the same object, based on its internal state. This can be a cleaner way for an object to change its behavior at runtime without resorting to conditional statements and thus improve maintainability.
+
+[Wikipedia: State Pattern](https://en.wikipedia.org/wiki/State_pattern)
---
+++ state/state.dart
@@ -0,0 +1,48 @@
+abstract class State {
+  void handler(Stateful context);
+  String toString();
+}
+
+class StatusOn implements State {
+  handler(Stateful context) {
+    print("  Handler of StatusOn is being called!");
+    context.state = StatusOff();
+  }
+
+  @override String toString() {
+    return "on";
+  }
+}
+
+class StatusOff implements State {
+	handler(Stateful context) {
+    print("  Handler of StatusOff is being called!");
+    context.state = StatusOn();
+  }
+
+  @override String toString() {
+    return "off";
+  }
+}
+
+class Stateful {
+  State _state;
+
+  Stateful(this._state);
+
+  State get state => _state;
+  set state(State newState) => _state = newState;
+
+  void touch() {
+    print("  Touching the Stateful...");
+    _state.handler(this);
+  }
+}
+
+void main() {
+  var lightSwitch = Stateful(StatusOff());
+  print("The light switch is ${lightSwitch.state}.");
+  print("Toggling the light switch...");
+  lightSwitch.touch();
+  print("The light switch is ${lightSwitch.state}.");
+}
