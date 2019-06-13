bb44a0ddf8ddcadcab3773689fb4be96593f3912
tyler <scottt2@uw.edu>
2019-06-13 08:45:30 -0600

    Whoops

--- factory_method/factory_method.dart
+++ factory_method/factory_method.dart
@@ -48,8 +48,8 @@ void main() {
   var sleepyVessel = CoffeeVesselFactory.vesselFor(Tiredness.sleepy);
   var kidVessel = CoffeeVesselFactory.vesselFor(Tiredness.hasChildren);
 
-  // A sleepy person would like a 500 milliliters cup full of coffee.
+  // A sleepy person would like a 500 milliliter cup full of coffee.
   print("A sleepy person would like $sleepyVessel.");
-  // A person with children NEEDS a 5 liters bucket full of coffee.
+  // A person with children NEEDS a 5 liter bucket full of coffee.
   print("A person with children NEEDS $kidVessel.");
 }
